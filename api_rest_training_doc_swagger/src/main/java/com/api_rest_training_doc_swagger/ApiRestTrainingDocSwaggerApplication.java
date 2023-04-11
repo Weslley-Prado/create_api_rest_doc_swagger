5cbb937e3943cb2945cc5f9bdec5511b2d65ef70
Weslley-Prado <weslley.prado73@gmail.com>
2023-04-11 20:09:33 -0300

    feat: create project

---
+++ api_rest_training_doc_swagger/src/main/java/com/api_rest_training_doc_swagger/ApiRestTrainingDocSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.api_rest_training_doc_swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestTrainingDocSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestTrainingDocSwaggerApplication.class, args);
+	}
+
+}
