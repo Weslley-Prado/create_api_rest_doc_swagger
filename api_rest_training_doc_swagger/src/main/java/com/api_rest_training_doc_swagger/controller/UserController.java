b25fce33a49f48298141f822f092283454c7f648
Weslley-Prado <weslley.prado73@gmail.com>
2023-04-11 22:07:38 -0300

    feat: creat new class and add swagger

---
+++ api_rest_training_doc_swagger/src/main/java/com/api_rest_training_doc_swagger/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.api_rest_training_doc_swagger.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api_rest_training_doc_swagger.model.User;
+import com.api_rest_training_doc_swagger.repository.UserRepository;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+	@Autowired
+	private UserRepository repository;
+	@GetMapping
+	public List<User>getUsers(){
+		return repository.findAll();
+	};
+	
+	@GetMapping("{username}")
+	public User getOne(@PathVariable("username") String username) {
+		return repository.findByUserName(username);
+	}
+	
+	@DeleteMapping("{id}")
+	public void deleteUser(@PathVariable("id")Integer id) {
+		repository.deleteById(id);
+	}
+	@PostMapping
+	public void postUser(@RequestBody User user) {
+		repository.save(user);
+	}
+	
+	@PutMapping()
+	public void putUser(@RequestBody User user) {
+		repository.save(user);
+	}
+
+}
---
+++ api_rest_training_doc_swagger/src/main/java/com/api_rest_training_doc_swagger/repository/UserRepository.java
@@ -0,0 +1,45 @@
+package com.api_rest_training_doc_swagger.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.api_rest_training_doc_swagger.model.User;
+
+@Repository
+public class UserRepository {
+	public void save(User user) {
+		if(user.getId()==null) 
+			System.out.println("Save - recebendo usuario na camada repository");
+		else
+			System.out.println("Update - recebendo usuario na camada repository");
+		
+		System.out.println(user);
+	}
+	
+	public void deleteById(Integer id) {		
+			System.out.println("Delete/id - recebendo usuario na camada repository");		
+			System.out.println(id);		
+	}
+	
+	public List<User> findAll(){
+		System.out.println("List - Listando todos os usuários");
+		List<User> users = new ArrayList<>();
+		users.add(new User("weslley", "password"));
+		users.add(new User("laisa", "masterpassword"));
+        return users;
+	}
+	
+	public User findById(Integer id) {
+		System.out.println(String.format("Find/id - Recebendo o id: %d", id));
+		return new User("weslley", "password");
+	}
+
+	public User findByUserName(String username) {
+		System.out.println(String.format("Find/username - Recebendo o id: %s", username));
+		return new User("weslley", "password");
+	}
+	
+
+}
